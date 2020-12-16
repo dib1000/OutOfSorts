914b3082bc572e75590858fd00c1a777e61a6e5f
Emma Buller <dib5017@gmail.com>
2020-12-16 18:38:30 -0500

    Tested more cases

--- Tester.java
+++ Tester.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Random;
 
 public class Tester {
   public static void main(String[] args) {
@@ -11,5 +12,42 @@ public class Tester {
     System.out.println(Arrays.toString(b));
     Sorts.bubbleSort(c);
     System.out.println(Arrays.toString(c));
+    System.out.println();
+
+    System.out.println("Testing misc arrays values");
+    int[] d = {};
+    Sorts.bubbleSort(d);
+    System.out.println(Arrays.toString(d));
+    int[] e = {16,17,19,16,30,16,45,30};
+    Sorts.bubbleSort(e);
+    System.out.println(Arrays.toString(e));
+    int[] sorted = {7,8,9,10,11,100};
+    int[] reverse = {100,11,10,9,8,7};
+    Sorts.bubbleSort(sorted);
+    Sorts.bubbleSort(reverse);
+    System.out.println(Arrays.toString(sorted));
+    System.out.println(Arrays.toString(reverse));
+    System.out.println();
+
+    System.out.println("Testing random lists");
+    for(int i = 0; i<15; i++) {
+      int ran = (int) (Math.random()*500);
+      Random rng = new Random(ran);
+      int len = (int)(Math.random()*100);
+      int[] x = new int[len];
+      for(int j = 0; j<len; j++) {
+        x[j] = rng.nextInt() % 1000;
+      }
+      int[] bx = x;
+      Arrays.sort(x);
+      Sorts.bubbleSort(bx);
+      if(Arrays.toString(x).equals(Arrays.toString(bx))) {
+        System.out.println("PASSED");
+      }
+      else {
+        System.out.println("FAIL");
+        System.out.println(ran);
+      }
+    }
   }
 }
